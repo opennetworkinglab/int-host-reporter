94e45ed283efe2267570e4d15f46a42582dca77b
Tomasz Osiński <tomasz@opennetworking.org>
2021-09-28 12:03:26 +0200

    test modifications to INT datapath

--- bpf/int-datapath.c
+++ bpf/int-datapath.c
@@ -66,9 +66,14 @@ struct seqno_ingress {
     __u16 dport;
 };
 
+struct shared_map_key = {
+    __u64 pkt_ptr;
+    __u32 flow_hash;
+};
+
 struct bpf_elf_map SEC("maps") SHARED_MAP = {
     .type = BPF_MAP_TYPE_HASH,
-    .size_key = sizeof(__u32),
+    .size_key = sizeof(struct shared_map_key),
     .size_value = sizeof(struct bridged_metadata),
     .pinning = PIN_GLOBAL_NS,
     .max_elem = 65536,
@@ -298,30 +303,35 @@ int ingress(struct __sk_buff *skb)
     bpf_printk("ip_src=%x, ip_dst=%x, proto=%d", bpf_htonl(ip_src), bpf_htonl(ip_dst), ip_protocol);
     bpf_printk("l4_sport=%d, l4_dport=%d", bpf_htons(l4_sport), bpf_htons(l4_dport));
 
-    __u16 flow_seqnum = 1;
-    struct seqno_ingress *val = bpf_map_lookup_elem(&SEQ_NO_INGRESS, &hash);
-    if (!val) {
-        struct seqno_ingress new_value = { };
-        new_value.seq_no = flow_seqnum;
-        new_value.ingress_timestamp = ingress_timestamp;
-        new_value.ingress_port = skb->ifindex;
-        new_value.ip_src = bpf_ntohl(ip_src);
-        new_value.ip_dst = bpf_ntohl(ip_dst);
-        new_value.protocol = ip_protocol;
-        new_value.sport = bpf_ntohs(l4_sport);
-        new_value.dport = bpf_ntohs(l4_dport);
-        bpf_map_update_elem(&SEQ_NO_INGRESS, &hash, &new_value, 0);
-    } else {
-        val->seq_no += 1;
-        val->ingress_timestamp = ingress_timestamp;
-        val->ingress_port = skb->ifindex;
-        val->ip_src = bpf_ntohl(ip_src);
-        val->ip_dst = bpf_ntohl(ip_dst);
-        val->protocol = ip_protocol;
-        val->sport = bpf_ntohs(l4_sport);
-        val->dport = bpf_ntohs(l4_dport);
-        flow_seqnum = val->seq_no;
-    }
+//    __u16 flow_seqnum = 1;
+//    struct seqno_ingress *val = bpf_map_lookup_elem(&SEQ_NO_INGRESS, &hash);
+//    if (!val) {
+//        struct seqno_ingress new_value = { };
+//        new_value.seq_no = flow_seqnum;
+//        new_value.ingress_timestamp = ingress_timestamp;
+//        new_value.ingress_port = skb->ifindex;
+//        new_value.ip_src = bpf_ntohl(ip_src);
+//        new_value.ip_dst = bpf_ntohl(ip_dst);
+//        new_value.protocol = ip_protocol;
+//        new_value.sport = bpf_ntohs(l4_sport);
+//        new_value.dport = bpf_ntohs(l4_dport);
+//        bpf_map_update_elem(&SEQ_NO_INGRESS, &hash, &new_value, 0);
+//    } else {
+//        val->seq_no += 1;
+//        val->ingress_timestamp = ingress_timestamp;
+//        val->ingress_port = skb->ifindex;
+//        val->ip_src = bpf_ntohl(ip_src);
+//        val->ip_dst = bpf_ntohl(ip_dst);
+//        val->protocol = ip_protocol;
+//        val->sport = bpf_ntohs(l4_sport);
+//        val->dport = bpf_ntohs(l4_dport);
+//        flow_seqnum = val->seq_no;
+//    }
+
+    struct shared_map_key key = {
+        .pkt_ptr = (__u64) skb,
+        .flow_hash = hash,
+    };
 
     struct bridged_metadata bmd = {
         .ingress_timestamp = ingress_timestamp,
@@ -333,7 +343,7 @@ int ingress(struct __sk_buff *skb)
         .seq_no = flow_seqnum,
     };
 
-    bpf_map_update_elem(&SHARED_MAP, &hash, &bmd, 0);
+    bpf_map_update_elem(&SHARED_MAP, &key, &bmd, 0);
 
     return TC_ACT_UNSPEC;
 }
@@ -367,7 +377,12 @@ int egress(struct __sk_buff *skb)
 
     bpf_printk("l4_sport=%d, l4_dport=%d", bpf_htons(udp->source), bpf_htons(udp->dest));
 
-    struct bridged_metadata *b = bpf_map_lookup_elem(&SHARED_MAP, &hash);
+    struct shared_map_key key = {
+        .pkt_ptr = (__u64) skb,
+        .flow_hash = hash,
+    };
+
+    struct bridged_metadata *b = bpf_map_lookup_and_delete_elem(&SHARED_MAP, &key);
     if (!b) {
         return TC_ACT_UNSPEC;
     }
@@ -378,30 +393,30 @@ int egress(struct __sk_buff *skb)
     bpf_printk("Hop latency=%llu, egress_port=%d, seq_no=%u", hop_latency, skb->ifindex,
                 b->seq_no);
 
-    bool drop_detected = false;
-    __u16 *last_seen_seqnum = bpf_map_lookup_elem(&EGRESS_LAST_SEEN_SEQNO, &hash);
-    if (!last_seen_seqnum) {
-        // first time we see this flow at egress
-        bpf_map_update_elem(&EGRESS_LAST_SEEN_SEQNO, &hash, &(b->seq_no), 0);
-    } else {
-        if (b->seq_no - *last_seen_seqnum > 1) {
-            drop_detected = true;
-        }
-        *last_seen_seqnum = b->seq_no;
-    }
-
-    if (drop_detected) {
-        struct dp_event evt = {};
-        evt.type = DP_EVENT_DROP;
-        evt.ingress_ifindex = b->ingress_port;
-        evt.pre_nat_ip_src = b->pre_nat_ip_src;
-        evt.pre_nat_ip_dst = b->pre_nat_ip_dst;
-        evt.pre_nat_sport = b->pre_nat_sport;
-        evt.pre_nat_dport = b->pre_nat_dport;
-        __u64 sample_size = skb->len < SAMPLE_SIZE ? skb->len : SAMPLE_SIZE;
-        bpf_perf_event_output(skb, &INT_EVENTS_MAP, (sample_size << 32) | BPF_F_CURRENT_CPU,
-                                  &evt, sizeof(evt));
-    }
+//    bool drop_detected = false;
+//    __u16 *last_seen_seqnum = bpf_map_lookup_elem(&EGRESS_LAST_SEEN_SEQNO, &hash);
+//    if (!last_seen_seqnum) {
+//        // first time we see this flow at egress
+//        bpf_map_update_elem(&EGRESS_LAST_SEEN_SEQNO, &hash, &(b->seq_no), 0);
+//    } else {
+//        if (b->seq_no - *last_seen_seqnum > 1) {
+//            drop_detected = true;
+//        }
+//        *last_seen_seqnum = b->seq_no;
+//    }
+//
+//    if (drop_detected) {
+//        struct dp_event evt = {};
+//        evt.type = DP_EVENT_DROP;
+//        evt.ingress_ifindex = b->ingress_port;
+//        evt.pre_nat_ip_src = b->pre_nat_ip_src;
+//        evt.pre_nat_ip_dst = b->pre_nat_ip_dst;
+//        evt.pre_nat_sport = b->pre_nat_sport;
+//        evt.pre_nat_dport = b->pre_nat_dport;
+//        __u64 sample_size = skb->len < SAMPLE_SIZE ? skb->len : SAMPLE_SIZE;
+//        bpf_perf_event_output(skb, &INT_EVENTS_MAP, (sample_size << 32) | BPF_F_CURRENT_CPU,
+//                                  &evt, sizeof(evt));
+//    }
 
     if (filter_allow(b->ingress_port, skb->ifindex, hash, egress_timestamp, hop_latency)) {
         struct dp_event evt = {};
