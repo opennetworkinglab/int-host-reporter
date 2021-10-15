2ea6e50a60199cd6128649d8d4e950d142134290
tosinski <tomasz@opennetworking.org>
2021-10-15 15:13:24 +0200

    Add bpf/flags.h file

---
+++ bpf/flags.h
@@ -0,0 +1,11 @@
+// Copyright 2021-present Open Networking Foundation
+// SPDX-License-Identifier: GPL-2.0-only
+
+/* Bridged metadata mode */
+#define BMD_MODE_SKB_CB 0
+#define BMD_MODE_SKB_PTR 1
+
+#ifndef BMD_MODE
+#define BMD_MODE BMD_MODE_SKB_CB
+#endif
+
