3aa00def089333267a34912928d947df8495fc7e
Tomasz Osiński <tomasz@opennetworking.org>
2021-10-25 10:53:08 +0200

    Make log level configurable (#28)

--- scripts/compile-bpf.sh
+++ scripts/compile-bpf.sh
@@ -3,4 +3,5 @@
 
 # Flags:
 # -DBMD_MODE=<BMD_MODE_SKB_PTR | BMD_MODE_SKB_CB | BMD_MODE_FLOW_HASH>, default=BMD_MODE_SKB_CB
+# -DDEBUG
 clang-10 -O2 -emit-llvm -g -c bpf/int-datapath.c -o - | llc-10 -march=bpf -filetype=obj -o /opt/out.o
